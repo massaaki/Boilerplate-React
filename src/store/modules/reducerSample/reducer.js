a28d741964e9e1d60c20d6a8844f60ac246e0b0a
Mauricio Yochizumi <mauricioyochizumi@MacBook-Pro-de-Mauricio-7.local>
2020-04-28 19:13:45 -0300

    adding redux

---
+++ src/store/modules/reducerSample/reducer.js
@@ -0,0 +1,5 @@
+function reducerSample() {
+  return [];
+}
+
+export default reducerSample;
---
+++ src/store/modules/rootReducer.js
@@ -0,0 +1,15 @@
+import { combineReducers } from 'redux';
+
+
+/**
+ * REDUCERS
+ */
+import reducerSample from './reducerSample/reducer';
+// -
+// End - REDUCERS
+
+
+export default combineReducers({
+  reducerSample,
+  // -
+});
