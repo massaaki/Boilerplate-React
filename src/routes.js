761ff375c507b201c2baf4df5b1cee2a999decf6
Mauricio Yochizumi <mauricioyochizumi@MacBook-Pro-de-Mauricio-7.local>
2020-04-28 15:50:20 -0300

    adding Cart page

--- src/routes.js
+++ src/routes.js
@@ -3,15 +3,18 @@ import { Switch, Route } from 'react-router-dom';
 
 /**
  * <> Define your pages here
+ * You can use snippet rfc to create a functional component
  */
-import Home from './pages/home';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+
 //* </> End Pages
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
-      {/* <Route path="/PATH" component={PAGE_NAME} /> */}
+      <Route path="/cart" component={Cart} />
     </Switch>
   );
 }
