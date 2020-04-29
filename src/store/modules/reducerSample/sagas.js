41df5f56cc30b4e12b385d7a3d3a5adb712f32ad
Mauricio Yochizumi <mauricioyochizumi@MacBook-Pro-de-Mauricio-7.local>
2020-04-29 10:25:37 -0300

    adding sagas.js in module

---
+++ src/store/modules/reducerSample/sagas.js
@@ -0,0 +1,13 @@
+import { call, put, all, takeLatest } from 'redux-saga/effects';
+import api from '../../../services/api';
+
+// import { addItemSuccess } from './actions';
+
+// function* addItem(id) {
+//   const response = yield call( api.get, `/some-request-here/${id}`);
+//   yield put(addItemSuccess(response.data));
+// }
+
+export default all([
+  //takeLatest('@Item/ADD_REQEUST', addItem),
+]);
